ed709c7a8603984700526027aebbe06514fc7a30
Marco Azimonti <git@azimonti.com>
2025-05-03 23:31:49 +0900

    updated python interface

--- cpp/python_bindings/pybind11_add_p.py
+++ cpp/python_bindings/pybind11_add_p.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 '''
 /************************/
 /*   pybind11_add_p.py  */
@@ -62,6 +61,4 @@ def main():
 
 
 if __name__ == '__main__':
-    if sys.version_info[0] < 3:
-        raise RuntimeError('Must be using Python 3')
     main()
